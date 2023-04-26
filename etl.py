b48b8ecd170578ea9a2bef6b9ba3f91e4b3d0d3a
karmendes <lucaseqt@hotmail.com>
2023-04-26 08:55:47 -0300

    feat: create logic in spark

--- etl.py
+++ etl.py
@@ -21,11 +21,30 @@ df_conta = spark.read.jdbc(url=url, table="conta", properties={"user": user, "pa
 df_cartao = spark.read.jdbc(url=url, table="cartao", properties={"user": user, "password": password, "driver": driver})
 df_movimento = spark.read.jdbc(url=url, table="movimento", properties={"user": user, "password": password, "driver": driver})
 
-# Juntar os dados
-df_join = df_cartao.join(df_conta,col("id_conta") == col("id"),"inner").join(df_associado)
-df_join = df_join.join(df_cartao,col("id") == col("id_conta"),"inner")
-df_join = df_movimento.join(df_join,col("id") == col("id_cartao"),"left")
+# Substituindo os nomes dos ids
+df_conta = df_conta.withColumnRenamed('id', 'id_conta')
+df_cartao = df_cartao.withColumnRenamed('id', 'id_cartao')
+df_associado = df_associado.withColumnRenamed('id', 'id_associado')
 
+
+# Juntar os dados
+df_join = df_cartao.join(df_conta,["id_conta"],"inner")
+df_join = df_join.join(df_associado,["id_associado"],"inner")
+df_join = df_movimento.join(df_join,["id_cartao"],"left")
+
+# Selecionando colunas
+df_dest = df_join.select(
+    col("vlr_transacao").alias("vl_transacao_movimento"),
+    col("data_movimento"),
+    col("num_cartao"),
+    col("nom_impresso").alias("nom_impresso_cartao"),
+    col("tipo").alias("tipo_conta"),
+    col("data_criacao").alias("data_criacao_conta"),
+    col("nome").alias("nome_associado"),
+    col("sobrenome").alias("sobrenome_associado"),
+    col("idade").alias("idade_associado")
+    )
 
 # salva o resultado em um arquivo CSV
-#df.write.csv("arquivo.csv", header=True, mode="overwrite")
+df_dest.write.partitionBy('nome_associado').csv('data/')
+
